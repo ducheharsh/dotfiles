2ab4c7bcd62f821ff091aaa2001ffafe7c4b7a0e
Harsh Mahadev Duche <ducheharsh@gmail.com>
2024-10-09 09:44:58 +0530

    added: my-nvim-config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,14 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.updatetime = 50
+--Word Wrap config
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.showbreak =true
+vim.o.wrapmargin = 2
+--
